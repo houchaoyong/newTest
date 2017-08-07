9cbfd46b1edbd975cf05ae380156110bea8d2ca2
houchaoyong <344125851qq.com>
2017-08-07 16:13:06 +0800

    add test submit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.tv);
+        textView.setText("sss");
+    }
+}
