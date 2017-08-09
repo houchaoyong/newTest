2bb6c4d8d516db2b2951be57f803e74f0565494c
houchaoyong <344125851qq.com>
2017-08-09 10:21:32 +0800

    修改hello he新增布局文件

--- app/src/main/java/com/example/administrator/myapplication/Hello.java
+++ app/src/main/java/com/example/administrator/myapplication/Hello.java
@@ -1,8 +1,17 @@
 package com.example.administrator.myapplication;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Administrator on 2017/8/9 0009.
  */
 
-public class Hello {
+public class Hello extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
 }
